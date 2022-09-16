53ebce80d2b152c4bca0e9fee65db5bea0a682ee
worldofmarcus <me@marcuseriksson.com>
2022-09-16 08:28:43 +0000

    Change requirements.txt

--- womblog/settings.py
+++ womblog/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["womblog.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["worldofmarcus-blog.herokuapp.com", "localhost"]
 
 
 # Application definition
